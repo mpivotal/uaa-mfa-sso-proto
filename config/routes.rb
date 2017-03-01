52f308d79825b89b538446e42f36f623c3c06802
Mike Long <mlong@pivotal.io>
2017-03-01 11:12:56 -0800

    we're cooking with gas

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   get 'main/sign_in'
 
-  get 'main/install'
+  get '/install', to: 'main#install'
 
-  get 'main/barcode_setup'
+  get '/barcode_setup', to: 'main#barcode_setup'
 
-  get 'main/manual_setup'
+  get '/manual_setup', to: 'main#manual_setup'
 
-  get 'main/passcode_setup'
+  get '/passcode_setup', to: 'main#passcode_setup'
 
-  get 'main/success'
+  get '/success', to: 'main#sign_in'
 
   root 'main#sign_in'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
