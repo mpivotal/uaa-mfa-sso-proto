8c6c8ead6a5d4e0519c9609a540eb625853ebbfa
Mike Long <mlong@pivotal.io>
2017-03-02 12:22:09 -0800

    meeeeewwwwwuuurrr

--- app/assets/javascripts/main.coffee
+++ app/assets/javascripts/main.coffee
@@ -1,3 +1,1 @@
-$ ->
-  if $('body.main.sign_in').length > 0
-    console.log "You're on the sign in page!"
+# nothing here
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get '/verify-passcode', to: 'main#passcode_setup'
 
-  get '/success', to: 'main#sign_in'
+  get '/success', to: 'main#success'
 
   root 'main#sign_in'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
